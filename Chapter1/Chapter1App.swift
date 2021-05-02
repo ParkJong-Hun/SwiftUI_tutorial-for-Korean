f285f0ab9a47ed6c985f82dac6d03a3f7891177e
pjh <thesn515@gmail.com>
2021-05-02 12:05:10 +0900

    Initial Commit

---
+++ Chapter1/Chapter1App.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter1App.swift
+//  Chapter1
+//
+//  Created by 박종훈 on 2021/05/02.
+//
+
+import SwiftUI
+
+@main
+struct Chapter1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chapter1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chapter1
+//
+//  Created by 박종훈 on 2021/05/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
